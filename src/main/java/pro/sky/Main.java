c82f6c64b117b7fdb80193a85888781ffe14a6b2
Atrilos <101889118+Atrilos@users.noreply.github.com>
2022-06-23 02:44:40 +0700

    init

--- src/main/java/pro/sky/Main.java
+++ src/main/java/pro/sky/Main.java
@@ -1,6 +1,88 @@
 package pro.sky;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println(task7("aabccddefgghiijjkk"));
+    }
 
+    public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+    }
+
+    public static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println(fullName.toUpperCase());
+    }
+
+    public static void task3() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = Arrays.stream(fullName.split(""))
+                .map(s -> {
+            if (s.equals("ё")) return "е";
+            else return s;
+        })
+                .collect(Collectors.joining());
+        System.out.println(fullName);
+    }
+
+    public static void task4(String s) {
+        String firstName = s.substring(0, s.indexOf(" "));
+        String middleName = s.substring(s.indexOf(" ") + 1, s.lastIndexOf(" "));
+        String lastName = s.substring(s.lastIndexOf(" ") + 1);
+        System.out.printf("""
+                Имя сотрудника - %s
+                Фамилия сотрудника - %s
+                Отчество сотрудника - %s%n
+                """, firstName, middleName, lastName);
+    }
+
+    public static String task5(String s) {
+        return Arrays
+                .stream(s.split(" "))
+                .map(str -> str.toUpperCase().charAt(0) + str.substring(1))
+                .collect(Collectors.joining(" "));
+    }
+
+    public static String task6(String s1, String s2) {
+        int pointer1 = 0, pointer2 = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (pointer1 < s1.length() && pointer2 < s2.length()) {
+            if (s1.charAt(pointer1) < s2.charAt(pointer2)) {
+                sb.append(s1.charAt(pointer1++));
+            } else {
+                sb.append(s2.charAt(pointer2++));
+            }
+        }
+
+        if (pointer1 == s1.length()) {
+            sb.append(s2.substring(pointer2));
+        } else {
+            sb.append(s1.substring(pointer1));
+        }
+
+        return sb.toString();
+    }
+
+    public static String task7(String s) {
+        LinkedHashSet<Character> set = new LinkedHashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0 && s.charAt(i - 1) == s.charAt(i)) {
+                set.add(s.charAt(i));
+            }
+        }
+
+        return set.stream().map(Objects::toString).collect(Collectors.joining());
+    }
 }
--- src/main/java/pro/sky/Optionals.java
+++ src/main/java/pro/sky/Optionals.java
@@ -1,4 +1,113 @@
 package pro.sky;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Optionals {
+    public static String task1(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        Arrays.stream(input.split("")).forEach(s -> sb.append(s).append(s));
+
+        return sb.toString();
+    }
+
+    public static String task2(String brackets, String input) {
+        StringBuilder sb = new StringBuilder();
+        String beginning = brackets.substring(0, brackets.length() / 2);
+        String ending = brackets.substring(brackets.length() / 2);
+
+        sb.append(beginning).append(input).append(ending);
+
+        return sb.toString();
+    }
+
+    public static String task3(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        if (a.isEmpty()) {
+            sb.append('@');
+        } else {
+            sb.append(a.charAt(0));
+        }
+
+        if (b.isEmpty()) {
+            sb.append('@');
+        } else {
+            sb.append(b.charAt(b.length() - 1));
+        }
+
+        return sb.toString();
+    }
+
+    public static String task4(String input) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            if (i >= input.length() - 2) {
+                if (i == input.length() - 2) {
+                    sb.append(input.charAt(i)).append(input.charAt(i + 1));
+                }
+            } else {
+                sb.append(input.charAt(i));
+            }
+        }
+        return sb.reverse().toString();
+    }
+
+    public static String task4(String s1, String s2) {
+        StringBuilder sb = new StringBuilder();
+        int minLength = Math.min(s1.length(), s2.length());
+
+        if (minLength == s1.length()) {
+            s2 = s2.substring(s2.length() - minLength);
+        } else {
+            s1 = s1.substring(s1.length() - minLength);
+        }
+        return sb.append(s1).append(s2).toString();
+    }
+
+    public static int task5(String input) {
+        int count = 0;
+        Matcher m = Pattern.compile("hi").matcher(input);
+
+        while (m.find()) {
+            ++count;
+        }
+        return count;
+    }
+
+    public static int task6(String input) {
+        int maxCount = 0;
+        Character last = null;
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            if (last == null) {
+                last = input.charAt(i);
+                count = 1;
+            } else if (last.equals(input.charAt(i))) {
+                ++count;
+            } else {
+                last = input.charAt(i);
+                maxCount = Math.max(count, maxCount);
+                count = 1;
+            }
+        }
+        return maxCount;
+    }
+
+    public static String task7(String word, String separator, int repeats) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < repeats; i++) {
+            sb.append(word);
+            if (i != repeats - 1) {
+                sb.append(separator);
+            }
+        }
+
+        return sb.toString();
+    }
 }
